dcf29e72cf2ca51c09ffc8314575ed0f05044e61
Seongguk Park <126008521+SeonggukPark@users.noreply.github.com>
2025-04-09 01:22:11 +0900

    Title: 꼬리잡기놀이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250409/꼬리잡기놀이/tail-catch-play.cpp
+++ 250409/꼬리잡기놀이/tail-catch-play.cpp
@@ -96,12 +96,14 @@ POS find_cluster(POS p) {
     POS head = { p.r, p.c }, tail = { p.r, p.c };
     visited[p.r][p.c] = 1, people[p.r][p.c] = team_idx;
 
+    int cnt = 1;
     while (true) {
         for (int dir = 0; dir < 4; ++dir) {
             int nr = tail.r + dr[dir], nc = tail.c + dc[dir];
             if (!is_grid({ nr, nc }) || visited[nr][nc] == 1 || grid[nr][nc] == ROAD || grid[nr][nc] == EMPTY) continue;
+            if (grid[nr][nc] == TAIL && cnt == 1) continue;
 
-            visited[nr][nc] = 1, people[nr][nc] = team_idx, tail = { nr, nc };
+            visited[nr][nc] = 1, people[nr][nc] = team_idx, tail = { nr, nc }, cnt++;
             if (grid[nr][nc] == OTHER) break;
             if (grid[nr][nc] == TAIL) return {nr, nc};
         }
@@ -139,8 +141,11 @@ POS find_next_pos(int idx, bool is_head) {
     for (int i = 0; i < 4; ++i) {
         int nr = cur.r + dr[i], nc = cur.c + dc[i];
         if (!is_grid({ nr, nc })) continue;
+        if (!is_head && (nr == teams[idx].h.r && nc == teams[idx].h.c)) continue;
+
         if ((is_head && people[nr][nc] == EMPTY && grid[nr][nc] == ROAD)
-            || (!is_head && people[nr][nc] != 0 && grid[nr][nc] == ROAD)) return {nr, nc};
+            || (!is_head && people[nr][nc] != 0 && grid[nr][nc] == ROAD )
+            || (is_head && nr == teams[idx].t.r && nc == teams[idx].t.c)) return {nr, nc};
     }
 
     return cur;
@@ -149,13 +154,23 @@ POS find_next_pos(int idx, bool is_head) {
 void move_teams() {
     // 팀 순회
     for (int team = 1; team < teams.size(); ++team) {
+        // cout << "Team " << team << ": ";
+
         // 1. 머리, 꼬리 이동 방향 찾기
         POS nh = find_next_pos(team, true), nt = find_next_pos(team, false);
 
         // 머리, 꼬리 정보 갱신 + people 격자 반영
         POS& h = teams[team].h, & t = teams[team].t;
-        people[t.r][t.c] = 0;
 
+        //cout << h.r << ' ' << h.c << ' ' << t.r << ' ' << t.c << " → ";
+         //cout << nh.r << ' ' << nh.c << ' ' << nt.r << ' ' << nt.c << endl;
+
+        if (nh.r == t.r && nh.c == t.c) {
+            h = nh, t = nt;
+            continue;
+        }
+
+        people[t.r][t.c] = 0;
         h = nh, t = nt;
         people[h.r][h.c] = team;
     }
@@ -174,28 +189,27 @@ void throw_ball() {
     // 공 발사
     while (is_grid(cur_ball) && people[cur_ball.r][cur_ball.c] == 0) {
         cur_ball.r += dr[dir], cur_ball.c += dc[dir];
+
     }
 
     fill_2d_int(visited, 0);
 
-
     if (is_grid(cur_ball)) { // 사람 맞은 경우 그룹 내 순서 찾고 점수화
         POS cur = teams[people[cur_ball.r][cur_ball.c]].h;
         visited[cur.r][cur.c] = 1;
-        int idx = 1;
-
+        int idx = 1, cnt = 1;
         while (cur.r != cur_ball.r || cur.c != cur_ball.c) {
-            
-            // cout << cur.r << ' ' << cur.c << endl;
             for (int i = 0; i < 4; ++i) {
                 int nr = cur.r + dr[i], nc = cur.c + dc[i];
-                if(!is_grid({nr, nc}) || people[nr][nc] == 0 || grid[nr][nc] != ROAD || visited[nr][nc] == 1) continue;
+                if (!is_grid({ nr, nc }) || people[nr][nc] == 0 || grid[nr][nc] != ROAD || visited[nr][nc] == 1) continue;
                 cur = { nr, nc }, visited[nr][nc] = 1;
                 break;
             }
 
             idx++;
         }
+        
+        // cout << "Hit at " << cur_ball.r << ' ' << cur_ball.c << " whose idx is " << idx << " of groud " << people[cur_ball.r][cur_ball.c] << endl;
         acc_score += (idx * idx);
 
         POS& h = teams[people[cur_ball.r][cur_ball.c]].h, &t = teams[people[cur_ball.r][cur_ball.c]].t;
@@ -206,10 +220,14 @@ void throw_ball() {
 void run() {
     cluster_teams(); // 팀 클러스터링 + 격자 재 설정
 
+    //traverse_2d_int(grid);
+    //traverse_2d_int(people);
     while (K--) {
         cur_turn++;
         move_teams(); // 1. 각 팀은 머리사람 따라 한 칸 이동
         throw_ball(); // 2. 공 투척
+        //traverse_2d_int(people);
+
     }
 
     // 각 팀이 얻은 점수 총합 출력
