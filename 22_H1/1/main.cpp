f7f79a953fd36066561af3088e9c51c296906d15
Seongguk Park <psk2013@naver.com>
2026-02-09 00:59:37 +0900

    first commit

---
+++ 22_H1/1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    freopen("input.txt", "r", stdin);
+    string str; cin >> str; cout << str;
+    return 0;
+}
